8627939bb406fdc38745ef17890e369e3b53dcbb
Artem Smirnov <artemsmirnov@list.ru>
2021-02-21 23:17:48 +0300

    game was finished

--- scripts.js
+++ scripts.js
@@ -7,6 +7,26 @@ const pairsNumField = document.getElementById('cells-num');
 const gameArea = document.getElementById('game-area');
 // кнопка запуска игры
 const startBtn = document.getElementById('start-game-btn');
+// ограничение на время игры в секундах
+const GAME_TIME = 60;
+
+// данные текущей игры
+const game = {
+  // флаг блокировки игрового поля
+  lockboard: false,
+  // есть ли на поле перевёрнутая костяшка не в состоянии найденной пары
+  hasFlippedCard: false,
+  // первая перевёрнутая костяшка на новой итерации поиска пары
+  firstCard: null,
+  // вторая перевёрнутая костяшка на новой итерации поиска пары
+  secondCard: null,
+  // переменная для игровых карточек текущей игры (DOM-элементы)
+  gameCards: [],
+  // флаг завершённой игры
+  finished: false,
+  // таймер текущей игры
+  timer: null
+};
 
 // шаблон для игровой костяшки
 const cardTemplate = `
@@ -15,9 +35,6 @@ const cardTemplate = `
     <img class='back-face' src='img/js-badge.svg' alt='Memory Card'>
   </div>
 `;
-
-// переменная для игровых карточек текущей игры (DOM-элементы)
-let gameCards = [];
 /*****************************************/
 
 // инициализация игры
@@ -36,19 +53,21 @@ function initGame() {
   shuffleCards();
 
   // навешиваем события на костяшки
-  gameCards.forEach( card => {
+  game.gameCards.forEach( card => {
     card.addEventListener('click', e => {
       flipCard(card);
     });
   });
+
+  // создаём таймер на игру
+  game.timer = setTimeout(showGameOverMessage, GAME_TIME * 1000);
 }
 
 // рандомное перемешивание костяшек игрового поля
 function shuffleCards() {
   // количество игровых костяшек
-  const cardsNum = gameCards.length;
-  gameCards.forEach( card => {
-    console.log(card);
+  const cardsNum = game.gameCards.length;
+  game.gameCards.forEach( card => {
     const randomPos = Math.floor( Math.random() * cardsNum);
     card.style.order = randomPos;
   });
@@ -56,10 +75,145 @@ function shuffleCards() {
 
 // переворачивание карточки card
 function flipCard(card) {
+  // если игровое поле залочено, выходим из функции
+  if(game.lockboard === true) {
+    return;
+  }
+
+  // если костяшка уже имеет пару на игровом поле, то клик
+  // по ней не обрабатываем
+  if(card.classList.contains('_finded')) {
+    return;
+  }
+
+  // если на новой итерации одна костяшка перевёрнута и юзер кликнул
+  // на неё же, то ничего не предпринимаем и выходим из функции
+  if(card === game.firstCard) {
+    return;
+  }
+
   // добавляем класс состояния перевёрнутой карточки
   card.classList.add('flip');
 
+  // если это первая перевёрнутая костяшка в новой итерации
+  if (!game.hasFlippedCard) {
+    game.hasFlippedCard = true;
+    game.firstCard = card;
+    return;
+  }
 
+  // далее считаем, что в новой итерации у нас перевёрнута вторая костяшка
+  game.secondCard = card;
+  // блокируем игровое поле на время проверки результат
+  game.lockboard = true;
+
+  // проверяем результаты игры
+  checkGameResults();
+}
+
+// проверка игрового хода и, в целом, состояния игры на завершённость
+function checkGameResults() {
+  // флаг совпадения пары костяшек
+  const isMatch =
+    game.firstCard.dataset.name === game.secondCard.dataset.name;
+
+  // если костяшки совпали
+  if(isMatch) {
+    // отмечаем костяшки найдеными
+    game.firstCard.classList.add('_finded');
+    game.secondCard.classList.add('_finded');
+    // обнуляем текущие костяшки игры
+    game.firstCard = null;
+    game.secondCard = null;
+
+    // флаг завершённой игры
+    game.finished = gameArea.querySelectorAll('.game-card._finded').length === game.gameCards.length;
+  }
+
+  // если игра завершена
+  if(game.finished) {
+    // показываем кнопку перезапуска игры
+    // таймаут выставляем, чтобы закончилась анимация разворачивания костяшки
+    setTimeout(showSuccessMessage, 1000);
+  }
+
+  // сбрасываем данные игрового шага
+  resetGameStage();
+}
+
+// показывает сообщение об успешном завершении игры
+function showSuccessMessage() {
+  // сообщение об успешном завершении игры
+  const successMessage = `
+    <div class='alert alert-secondary'>
+      <h4
+        class='alert-heading text-center'
+      >
+        Вы выиграли!
+      </h4>
+      <hr>
+      <p>
+        <button
+          type='button'
+          class='btn btn-secondary btn-lg text-nowrap'
+          onclick='initGame();'
+        >
+          Сыграть ещё раз
+        </button>
+      </p>
+    </div>
+  `;
+
+  // заменяем костяшки на сообщение
+  gameArea.innerHTML = successMessage;
+}
+
+// показывает сообщение о неудачном завершении игры
+function showGameOverMessage() {
+  // сообщение о неуспешном завершении игры
+  const gameOverMessage = `
+    <div class='alert alert-danger'>
+      <h4
+        class='alert-heading text-center'
+      >
+        Вы проиграли!
+      </h4>
+      <hr>
+      <p>
+        <button
+          type='button'
+          class='btn btn-danger btn-lg text-nowrap'
+          onclick='initGame();'
+        >
+          Попробовать ещё раз
+        </button>
+      </p>
+    </div>
+  `;
+
+  // заменяем костяшки на сообщение
+  gameArea.innerHTML = gameOverMessage;
+}
+
+// сбрасываем данные игрового шага
+function resetGameStage() {
+  setTimeout(() => {
+    // если развёрнута первая костяшка, то разворачиваем назад
+    if(game.firstCard) {
+      game.firstCard.classList.remove('flip');
+    }
+
+    // если развёрнута вторая костяшка, то разворачиваем назад
+    if(game.secondCard) {
+      game.secondCard.classList.remove('flip');
+    }
+
+    // обнуляем данные игрового шага
+    game.hasFlippedCard = false;
+    game.lockboard = false;
+    game.firstCard = null;
+    game.secondCard = null;
+  }, 1000);
 }
 
 // устанавливаем ширину игрового поля в зависимости от количества
@@ -91,7 +245,7 @@ function fillGameAreaWithPairs(pairsNum) {
   }
 
   // сохраняем карточки игрового поля в глобальную переменную
-  gameCards = gameArea.querySelectorAll('.game-card');
+  game.gameCards = gameArea.querySelectorAll('.game-card');
 }
 
 // добавление пары карточек на игровое поле
@@ -114,8 +268,15 @@ function resetGame() {
   gameArea.innerHTML = '';
   // сброс ширины игрового поля
   gameArea.style.width = '';
-  // удаляем все костяшки текущей игры
-  gameCards = [];
+  // сбрасываем состояние игры к дефолтному состоянию
+  game.finished = false;
+  game.lockboard = false;
+  game.hasFlippedCard = false;
+  game.firstCard = null;
+  game.secondCard = null;
+  game.gameCards = [];
+  // обнуляем таймер игры
+  clearTimeout(game.timer);
 }
 
 // навешиваем хендлеры при загрузке документа
@@ -131,21 +292,10 @@ window.addEventListener('load', () => {
 let cards = document.querySelectorAll('.card');
 
 let hasFlippedCard = false;
-let lockBoard = false;
+let lockboard = false;
 let firstCard, secondCard;
 let menu = document.querySelector('.section-menu')
 let countFlipCard = 4;
-
-function addCards(cardArr) {
-  cardArr.forEach(function(number) {
-    createCard(number)
-  });
-  cardArr.forEach(function(number) {
-    createCard(number)
-  });
-  cards = document.querySelectorAll('.card');
-}
-
 
 function pageReset () {
   menu.style.display = 'none';
@@ -162,27 +312,6 @@ function pageReset () {
   cards.forEach(card => card.addEventListener('click', flipCard));
 }
 
-// let btnReset = document.querySelector('.btn');
-// btnReset.addEventListener("click", pageReset);
-
-// function flipCard() {
-//   if (lockBoard) return;
-//   if (this === firstCard) return;
-//
-//   this.classList.add('flip');
-//
-//   if (!hasFlippedCard) {
-//     hasFlippedCard = true;
-//     firstCard = this;
-//     return;
-//   }
-//
-//   secondCard = this;
-//   lockBoard = true;
-//
-//   checkForMatch();
-// }
-
 function checkForMatch() {
   let isMatch = firstCard.dataset.name === secondCard.dataset.name;
   isMatch ? disableCards() : unflipCards();
@@ -196,31 +325,3 @@ function disableCards() {
   if (countFlipCard == 0 ) menu.style.display = 'block';
   resetBoard();
 }
-
-
-
-
-function unflipCards() {
-  setTimeout(() => {
-    firstCard.classList.remove('flip');
-    secondCard.classList.remove('flip');
-
-    resetBoard();
-  }, 1500);
-}
-
-function resetBoard() {
-  hasFlippedCard = false;
-  lockBoard = false;
-  firstCard = null;
-  secondCard = null;
-}
-
-// (function gameTime() {
-//   setTimeout(() => {
-//     menu.style.display = 'block';
-//     menu.style.backgroundColor = "#AA0000";
-//
-//   }, 60000);
-// })();
-
